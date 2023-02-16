297669946d04a4b0ba635742f7e15bf0bde1b152
Devynn R. Palmer <palmerdevynn@gmail.com>
2023-02-16 01:28:01 +0000

    subarray challenge

--- src/main/java/SubArray.java
+++ src/main/java/SubArray.java
@@ -13,6 +13,11 @@ public class SubArray {
      * @return a sub-array of nums containing the values between start and end.
      */
     public int[] sub(int[] nums, int start, int end){
-        return null;
+        int[] result = new int[end - start];
+        for(int i = 0; i < result.length; i++) {
+            result[i] = nums[start];
+            start++;
+        }
+        return result;
     }
 }
